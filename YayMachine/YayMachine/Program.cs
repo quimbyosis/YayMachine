970b9568d5780ca6eee54f08b028be3cb8436ac6
Quimby <laura_j_quimby@hotmail.com>
2016-04-26 15:42:38 -0400

    Adding it all in.

---
+++ YayMachine/YayMachine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YayMachine
+{
+	class Program
+	{
+		static void Main()
+		{
+
+				//Prompt user to enter number of yays
+				Console.Write("Enter the number of times to print \"Yay!\": ");
+
+				var entry = Console.ReadLine();
+
+				var yays = int.Parse(entry);
+
+		}
+	}
+}
