0e9d2a3872f126ebd349690ebe11b60bc9dbd7b9
Quimby <laura_j_quimby@hotmail.com>
2016-04-27 10:44:15 -0400

    Added input validation, and option to quit.

--- YayMachine/YayMachine/Program.cs
+++ YayMachine/YayMachine/Program.cs
@@ -8,17 +8,41 @@ namespace YayMachine
 		static void Main()
 		{
 
-			//Prompt user to enter number of yays
-			Console.Write("Enter the number of times to print \"Yay!\": ");
-
-			var entry = Console.ReadLine();
-
-			var yays = int.Parse(entry);
-			//yays is the number we need to extract to turn into yays
-
-			for (int i = 0; i != yays ; i++)
+			while (true)
 			{
-				Console.WriteLine("Yay!");
+				//Prompt user to enter number of yays
+				Console.Write("Enter the number of times to print \"Yay!\", or enter \"quit\": ");
+
+				var entry = Console.ReadLine();
+
+				if (entry.ToLower() == "quit")
+				{
+					Console.WriteLine("Later, bruh.");
+					break;
+				}
+
+				try
+				{
+					var yays = int.Parse(entry);
+					//yays is the number we need to extract to turn into yays
+
+					if (yays <= 0)
+					{
+						Console.WriteLine("You must enter a positive number.");
+					}
+
+					else
+					{
+						for (int i = 0; i != yays; i++)
+						{
+							Console.WriteLine("Yay!");
+						}
+					}
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("You must enter a whole number.");
+				} 
 			}
 
 		}
