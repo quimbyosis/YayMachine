f1b04e2fb055f9baa0aeb54fa80ab6b69deb79b6
Quimby <laura_j_quimby@hotmail.com>
2016-04-27 10:22:05 -0400

    Added for loop to print out Yay! for number of times someone enters in.

--- YayMachine/YayMachine/Program.cs
+++ YayMachine/YayMachine/Program.cs
@@ -4,15 +4,22 @@ namespace YayMachine
 {
 	class Program
 	{
+
 		static void Main()
 		{
 
-				//Prompt user to enter number of yays
-				Console.Write("Enter the number of times to print \"Yay!\": ");
+			//Prompt user to enter number of yays
+			Console.Write("Enter the number of times to print \"Yay!\": ");
 
-				var entry = Console.ReadLine();
+			var entry = Console.ReadLine();
 
-				var yays = int.Parse(entry);
+			var yays = int.Parse(entry);
+			//yays is the number we need to extract to turn into yays
+
+			for (int i = 0; i != yays ; i++)
+			{
+				Console.WriteLine("Yay!");
+			}
 
 		}
 	}
